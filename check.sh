8ccaf6185ddc182887d942575e2e300d06798e41
Maxwell Talbot <talbot.maxwell@gmail.com>
2022-05-23 13:06:21 +0100

    re enable submission to slack

--- check.sh
+++ check.sh
@@ -27,7 +27,7 @@ then
     echo "They match. all is good"
 else 
     SUBMODULE_LOCATION=$(git submodule | grep $REPO_NAME |awk '{print $2}' | sed -En 's/-*(.*)/\1/p')
-    
+
 
     git submodule set-url $SUBMODULE_LOCATION $SUBMODULE_GIT_URL
     git submodule update --init $SUBMODULE_LOCATION
@@ -39,13 +39,11 @@ else
     DIFF_URL=https://github.com/rive-app/$REPO_NAME/compare/$SUBMODULE_HASH...$REPO_HASH
     echo "git rev-list --left-right --count $SUBMODULE_HASH...$REPO_HASH"
     DIFF_COUNT=$(git fetch && git rev-list --left-right --count $SUBMODULE_HASH...$REPO_HASH | awk '{print $2}')
-    echo $DIFF_COUNT
-    echo $DIFF_URL
-
+    echo '{"text":" `'$NAME'` is behind `'$REPO_NAME'` by <'$DIFF_URL'|'$DIFF_COUNT' commit(s)>."}'
+    
     # Just full on assuming this is behind for the time being. 
     # Could put in some 'useful' links into this message. Once we have something useful to do (other than update & rebuild)
-    # curl -X POST -H 'Content-type: application/json' --data '{"text":"`'"$NAME"'` is behind '"$REPO_NAME"'!"}' $SLACK_WEBHOOK
-    # curl -X POST -H 'Content-type: application/json' --data '{"text":" `'$NAME'` is behind `'$REPO_NAME'` by <'$DIFF_URL'|'$DIFF_COUNT' commit(s)>."}' $SLACK_WEBHOOK
-    # echo '{"text":" `'$NAME'` is behind `'$REPO_NAME'` by <'$DIFF_URL'|'$DIFF_COUNT' commit(s)>."}'
+    curl -X POST -H 'Content-type: application/json' --data '{"text":" `'$NAME'` is behind `'$REPO_NAME'` by <'$DIFF_URL'|'$DIFF_COUNT' commit(s)>."}' $SLACK_WEBHOOK
+    
 
 fi 
